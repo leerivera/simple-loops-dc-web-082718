3982253a2d8057eb4c5e24c117247de2e8496666
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-08-23 16:10:34 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -24,7 +24,7 @@ def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   
   7.times do
-    puts "Welcome to Flatiron School's Web Developement Course!"
+    puts "Welcome to Flatiron School's Web Development Course!"
     
   end
   
